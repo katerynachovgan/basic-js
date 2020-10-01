62f0286d03988a9433969c0df5f2a2148e3ff538
Kateryna Chovhan <chovganek@gmail.com>
2020-10-02 00:24:47 +0300

    feat: add count-cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,14 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity ) {
+  
+  let result = 0;
+  let finalActivity = parseFloat(sampleActivity);
+  let k = (0.693 / HALF_LIFE_PERIOD).toFixed(6)
+  let lgr = Math.log(MODERN_ACTIVITY / finalActivity);
+  if ((sampleActivity) || ((sampleActivity !== String)) || (/^[:digit:]$/.test)) return false;
+
+  result = Math.ceil( lgr / k);
+  return result
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let number = 0;
+  let ar = backyard;
+  for (let i = 0; i < ar.length; i++){
+    for (let j = 0; j<ar[i].length; j++){
+      if (ar [i][j] === '^^'){
+        number++;
+      }
+    }
+  }
+  return number
 };
