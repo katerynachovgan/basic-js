2ed1961c90356a15e3a3f8471337b0f21ea07eb9
Kateryna Chovhan <chovganek@gmail.com>
2020-10-10 00:33:31 +0300

    feat: add hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
   // remove line with error and write your code here
+//   Реализуйте функцию `calculateHanoi`, которая принимает параметры `diskNumber` и 
+//   `turnsSpeed`. `diskNumber` — это число **дисков**, а `turnsSpeed` — скорость 
+//   перемещения дисков (в **ходах** в **час**). Оба параметра являются числами (тип `number`)
+
+// Функция `calculateHanoi` возвращает объект с 2 свойствами:
+// * `turns` (минимальное число (тип `number`) ходов, необходимое для решения головоломки)
+// * `seconds` (минимальное число (тип `number`) **секунд**, необходимое для решения
+//  головоломки при заданной скорости; должно быть целым числом, полученным в результате
+//   округления результата расчетов в меньшую **(floor)** сторону)
+let turns = Math.pow(2, disksNumber) - 1;
+let seconds = Math.trunc((turns * 3600)/turnsSpeed);
+return { turns, seconds}
+
 };
