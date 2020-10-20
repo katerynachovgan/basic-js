376eed7b0e89c8762a96007c95578b9826f26ab3
Kateryna Chovhan <chovganek@gmail.com>
2020-10-20 13:39:15 +0300

    feat: add transform array result

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth( arr) {
+    if(arr.length === 0) {
+      return 1;
+    }
+    if (arr.length === 1 && typeof(arr[0]) === 'number') return 1;
+    
+    for (let i = 0; i < arr.length; i++){
+      if (Array.isArray(arr[i])){
+        // console.log(arr[i]);
+        // console.log(Array.isArray(arr[i]));
+        return calculateDepth(arr[i]) + 1;
+      }
+    }
   }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+result = [];
+// array = arr;
+// throw new CustomError('Not implemented');
+// remove line with error and write your code here
+if (!Array.isArray(arr)) throw TypeError;
+if (arr === []) return [];
+
+let l = arr.length;
+let j = 0;
+
+for (let i = 0; i < l; i++){
+switch(arr[i]){
+
+  case '--double-next': 
+  if(i !== l-1){
+    result[j] = arr[i+1];
+    j++;
+  break;
+  } else {
+  return result;
+  } 
+
+case '--double-prev':
+if  (i !== 0){
+  result[j] = arr[i-1];
+  j++;}
+  break;
+
+case '--discard-next':
+    if (i === l-1){
+    return result;
+    }
+    if((arr[i+2] === '--discard-prev') || (arr[i+2] === '--double-prev')){
+    i++;
+    }
+  i++;
+  break;
+
+case '--discard-prev':
+if ( i !== 0){
+  result.pop();
+  j-=1;
+} 
+break;
+default: 
+  result[j] = arr[i];
+  j++;
+  break;
+}
+}
+return result;
 };
